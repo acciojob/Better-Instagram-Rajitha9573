adf3f49840617572ed5bd1fe6796119f60578fa1
Rajitha9573 <141991018+Rajitha9573@users.noreply.github.com>
2023-09-24 12:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const image1 = document.getElementById("div1");
+const image2 = document.getElementById("div2");
+image1.addEventListener("dragover", (event) => {
+	event.preventDefault();
+});
+
+const image1 = document.getElementById("div1");
+image1.addEventListener("drop", () => {
+	image1.appendChild(image2);
+	
+});
